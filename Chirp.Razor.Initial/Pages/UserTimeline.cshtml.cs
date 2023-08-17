fb6d63c707aa75ff5035bf34ec7741f8376339e1
Patrick Matthiesen <43612965+PatrickMatthiesen@users.noreply.github.com>
2023-08-17 16:01:11 +0200

    add razor template

---
+++ Chirp.Razor.Initial/Pages/UserTimeline.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Chirp.Razor.Initial.Pages;
+
+public class UserTimelineModel : PageModel
+{
+    private readonly CheepService _service;
+    public List<CheepViewModel> Cheeps { get; set; }
+
+    public UserTimelineModel(CheepService service)
+    {
+        _service = service;
+    }
+
+    public ActionResult OnGet(string author)
+    {
+        Cheeps = _service.GetCheepsFromAuthor(author);
+        return Page();
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# Templates for Razor and Blazor
+
+## Pack to NuGet package
+
+1. Make sure that you are in this folder
+2. Run: ```dotnet pack -o .```
+
+> If no output is set, then the file will be placed in `bin/Release`
