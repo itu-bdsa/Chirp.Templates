69c53ae8d986cbf6ff7fdbce7c7346cfe92ee5a5
Patrick Matthiesen <43612965+PatrickMatthiesen@users.noreply.github.com>
2023-08-17 16:01:11 +0200

    add razor template

---
+++ Chirp.Razor.Initial/Pages/Public.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Chirp.Razor.Initial.Pages;
+
+public class PublicModel : PageModel
+{
+    private readonly CheepService _service;
+    public List<CheepViewModel> Cheeps { get; set; }
+
+    public PublicModel(CheepService service)
+    {
+        _service = service;
+    }
+
+    public ActionResult OnGet()
+    {
+        Cheeps = _service.GetCheeps();
+        return Page();
+    }
+}
